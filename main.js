bc3df673c3e239c81270a801264dcd2bb78875a8
AshwiniChin <76168179+AshwiniChin@users.noreply.github.com>
2022-03-17 12:46:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 
 menu_list_array = ["Veg Margherita Pizza",
-"Veg Supreme Pizza",
-"Paneer Tikka Pizza",
-"Deluxe Veggie Pizza",
-"Veg Extravaganza Pizza"];
-    
-function getmenu(){
-    var htmldata="<ol class='menulist'>"
+    "Veg Supreme Pizza",
+    "Paneer Tikka Pizza",
+    "Deluxe Veggie Pizza",
+    "Veg Extravaganza Pizza"];
+
+function getmenu() {
+    var list = "<ol class='menulist'>";
 
     menu_list_array.sort();
 
-    for(var i=0;i<menu_list_array.length;i++){
-        htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+    for (var i = 0; i < menu_list_array.length; i++) {
+        list = list + '<li>' + menu_list_array[i] + '</li>';
     }
-     htmldata=htmldata+"</ol>"
-     document.getElementById("display_menu").innerHTML = htmldata;
-     document.getElementById("addTopings").style.display = "none";
+    list = list + "</ol>";
+    document.getElementById("display_menu").innerHTML = list;
+    document.getElementById("addTopings").style.display = "none";
 }
 
-function addTopings(){
+function addTopings() {
     document.getElementById("addTopings").style.display = "block";
 }
 
-function addItem(){
-    var item=document.getElementById("item").value;
+function addItem() {
+    var item = document.getElementById("item").value;
     menu_list_array.push(item);
     getmenu();
-    }
+}
